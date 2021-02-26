cfe9dabe2f8e8efece811c5b8b677beb343de10d
Shauna Dunn <shaunadunn1@gmail.com>
2021-02-26 15:35:19 -0500

    added skeletons to handlebars files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,17 @@ const router = express.Router();
 // Import the model (burger.js) to use its database functions.
 const burger = require('../models/burger.js');
 
+// Create all our routes and set up logic within those routes where required.
+router.get('/', (req, res) => {
+    burger.all((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+  
+
 
 module.exports = router;
