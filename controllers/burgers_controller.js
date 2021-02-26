14fa66bed27dd7653727378ce343ff5de1840019
Shauna Dunn <shaunadunn1@gmail.com>
2021-02-26 15:52:25 -0500

    adding code from catapp as example

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,6 +15,45 @@ router.get('/', (req, res) => {
       res.render('index', hbsObject);
     });
   });
+
+  router.post('/api/burgers', (req, res) => {
+    burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+  
+  router.delete('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    cat.delete(condition, (result) => {
+      if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    });
+  });
   
 
 
