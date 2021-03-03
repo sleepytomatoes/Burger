f3cd3ebdb27c446e541077733fcc11a285a406a6
Shauna Dunn <shaunadunn1@gmail.com>
2021-03-02 21:30:24 -0500

    app routing working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,17 +7,18 @@ const burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
 router.get('/', (req, res) => {
-    burger.all((data) => {
+    burger.selectAll((data) => {
       const hbsObject = {
         burgers: data,
       };
-      console.log(hbsObject);
+      console.log("hbsObject", hbsObject);
       res.render('index', hbsObject);
     });
   });
 
   router.post('/api/burgers', (req, res) => {
-    burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+    console.log("in the post method")
+    burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
@@ -28,10 +29,7 @@ router.get('/', (req, res) => {
   
     console.log('condition', condition);
   
-    burger.update(
-      {
-        burger_name: req.body.name
-      },
+    burger.updateOne(
       {
         devoured: req.body.devoured,
       },
@@ -49,7 +47,7 @@ router.get('/', (req, res) => {
   router.delete('/api/burgers/:id', (req, res) => {
     const condition = `id = ${req.params.id}`;
   
-    burger.delete(condition, (result) => {
+    burger.deleteOne(condition, (result) => {
       if (result.affectedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
--- models/burger.js
+++ models/burger.js
@@ -3,17 +3,18 @@ const orm = require('../config/orm.js');
 
 const burger = {
     selectAll(cb) {
-      orm.all('burgers', (res) => cb(res));
+      orm.selectAll('burgers', (res) => cb(res));
     },
     // The variables cols and vals are arrays.
     insertOne(cols, vals, cb) {
-      orm.create('burgers', cols, vals, (res) => cb(res));
+      orm.insertOne('burgers', cols, vals, (res) => cb(res));
     },
     updateOne(objColVals, condition, cb) {
-      orm.update('burgers', objColVals, condition, (res) => cb(res));
+        console.log("trying to update")
+      orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
     },
     deleteOne(condition, cb) {
-      orm.delete('burgers', condition, (res) => cb(res));
+      orm.deleteOne('burgers', condition, (res) => cb(res));
     },
   };
   
