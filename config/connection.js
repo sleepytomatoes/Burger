db80fd4c3adcd6f4be0b6b7e650c09b33040017b
Shauna Dunn <shaunadunn1@gmail.com>
2021-03-03 21:00:05 -0500

    heroku changes

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'sexywhore',
+  password: process.env.password,
   database: 'burgers_db',
 });
 
